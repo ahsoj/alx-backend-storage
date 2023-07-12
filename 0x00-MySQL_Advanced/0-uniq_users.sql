51f0eabe07d919661ca96a652b1365136e58de5d
eyasu chaka <daa282386@gmail.com>
2023-07-12 16:05:06 +0000

    advanced_sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
 -- create a table
 -- add not null and unique constraints
 CREATE TABLE IF NOT EXISTS users(
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL,
 	name VARCHAR(255),
-	PRIMARY KEY (id)
 )
