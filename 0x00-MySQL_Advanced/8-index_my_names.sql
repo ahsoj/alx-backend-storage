645e13b0d855f0648cb9f40f0d017e897c8dbd73
eyasu chaka <daa282386@gmail.com>
2023-07-13 11:50:32 +0000

    advanced_sql

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,3 +1,3 @@
 --create am index `idx-name_first` on the table `names`
 -- and the first letter of `name`.
-CREATE INDEX idx_name_first on names(name(1))
+CREATE INDEX idx_name_first ON names(name(1))
