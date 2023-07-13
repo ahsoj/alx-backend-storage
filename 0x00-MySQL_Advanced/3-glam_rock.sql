5dfb3e91e1df320a10cc8952be33ba7106fe6e3c
eyasu chaka <daa282386@gmail.com>
2023-07-13 09:09:59 +0000

    advanced_sql

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- list all bands with `Glam rock` as their main style
+-- rannked by their longevity
+SELECT `band_name`, COALESCE(`split`, 2022) - `formed` as `lifespan`
+FROM `metal_bands`
+WHERE style LIKE '%Glam rock%'
+ORDER BY `lifespan` DESC;
