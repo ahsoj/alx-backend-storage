7b376011b359b7a48ab819ed90f2ec8ee5be8c91
eyasu chaka <daa282386@gmail.com>
2023-07-13 08:02:21 +0000

    advnaced_sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,8 @@
 -- create a table
 -- add not null and unique constraints
 CREATE TABLE IF NOT EXISTS users(
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
-	country ENUM('US', 'CO', 'TN') DEFAULT "US" NOT NULL,
-	PRIMARY KEY (id),
+	country ENUM('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
 )
