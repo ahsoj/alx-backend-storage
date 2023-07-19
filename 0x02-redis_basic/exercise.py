123b89a561e7886bb4b1e489c375b3eba104058e
eyasuc <mkeyasuc@gmail.com>
2023-07-19 15:10:10 +0300

    redis_server

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Class Module"""
+import redis
+from uuid import uuid4
+from typing import Union
+
+
+class Cache(object):
+    '''represent Cache class'''
+    def __init__(self):
+        '''Initialize instance'''
+        self._redis = redis.Redis()
+        self._redis.flushdb(True)
+
+    def store(self, data: Union[str, bytes, int, float]):
+        '''
+        class method takesa `data` arguments
+        rType: str
+        '''
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
